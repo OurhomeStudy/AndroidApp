572fa3aa015ee42c2ce25b09bf4a5ec7ae1d575c
ldg1291 <ldg1291@naver.com>
2015-10-02 15:17:22 +0900

    151002 / Dong / 2차

---
+++ app/src/main/java/Items/Review.java
@@ -0,0 +1,42 @@
+package Items;
+
+/**
+ * Created by Dong_Gyo on 2015. 10. 2..
+ */
+public class Review {
+
+    private String name;
+    private String content;
+    private int reviewstar;
+
+    public Review(String name, String content, int reviewstar) {
+        this.name = name;
+        this.content = content;
+        this.reviewstar = reviewstar;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    public void setContent (String content) {
+        this.content = content;
+    }
+
+    public void setReviewstar (int reviewstar) {
+        this.reviewstar = reviewstar;
+    }
+
+    public String getName () {
+        return name;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public int getReviewstar() {
+        return reviewstar;
+    }
+
+}
