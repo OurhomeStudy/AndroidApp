d5662432839ba892e70f1e1509604beeb83a1eed
ldg1291 <ldg1291@naver.com>
2015-10-02 15:07:36 +0900

    151002 / Dong / 1차

---
+++ app/src/main/java/Customize/ReviewAdapter.java
@@ -0,0 +1,37 @@
+package Customize;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by Dong_Gyo on 2015. 10. 2..
+ */
+public class ReviewAdapter extends BaseAdapter {
+
+    Context con;
+    LayoutInflater inflater;
+    int layout;
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
